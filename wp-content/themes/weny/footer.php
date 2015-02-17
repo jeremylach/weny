d3ae5c2b5506e51dc84593b3eeda3ecd7a46baec
Jeremy Lach <lach.jeremy@gmail.com>
2015-02-17 13:24:17 -1000

    Front end homepage

--- wp-content/themes/weny/footer.php
+++ wp-content/themes/weny/footer.php
@@ -1,52 +1,20 @@
 		</div><!-- #main -->
 
-		<footer id="colophon" class="site-footer row" role="contentinfo">
+		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info">
+				<div class='col-md-8 col-xs-8'>
 				&copy; <?php echo date("Y"); ?> Watch Enthusiasts of New York | <a href=''>Contact Us</a> | Site by <a href='http://iconinteractive.com'>ICON Interactive</a>
 			<!-- .site-info -->
-			
-				<?php wp_nav_menu(array('theme_location' => 'socialnav', 'menu_class' => 'navbar-nav', 'container' => false, 'items_wrap' => '<ul class="social">%3$s</ul>')); ?>
+				</div>
+				<div class='col-md-4'>
+					<?php wp_nav_menu(array('theme_location' => 'socialnav', 'menu_class' => 'navbar-nav', 'container' => false, 'items_wrap' => '<ul class="social">%3$s</ul>')); ?>
+				</div>
 			</div>
 		</footer><!-- #colophon -->
 
 
 	</div><!-- #page -->
 
-	<!--	<div class='signup-popup' role="dialog" id='mc-signup' aria-labelledby="myModalLabel" aria-hidden="true">-->
-		<!-- Begin MailChimp Signup Form -->
-		<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
-		<style type="text/css">
-			#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-			/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-		</style>
-		<div id="mc_embed_signup" class='signup-popup' role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style='display: none'>
-		<form action="//bringaloupe.us9.list-manage.com/subscribe/post?u=e998c2852efba0d747720e54b&amp;id=bb1ee6444a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-			<h2>Subscribe to the Bring a Loupe mailing list</h2>
-		<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-		<div class="mc-field-group">
-			<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-		</label>
-			<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-		</div>
-		<div class="mc-field-group">
-			<label for="mce-FNAME">First Name </label>
-			<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-		</div>
-		<div class="mc-field-group">
-			<label for="mce-LNAME">Last Name </label>
-			<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-		</div>
-			<div id="mce-responses" class="clear">
-				<div class="response" id="mce-error-response" style="display:none"></div>
-				<div class="response" id="mce-success-response" style="display:none"></div>
-			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-			<div style="position: absolute; left: -5000px;"><input type="text" name="b_e998c2852efba0d747720e54b_bb1ee6444a" tabindex="-1" value=""></div>
-			<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-		</form>
-		</div>
-		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-		<!--End mc_embed_signup-->
 	<!--</div>-->
 
 	<?php wp_footer(); ?>
--- wp-content/themes/weny/page-home.php
+++ wp-content/themes/weny/page-home.php
@@ -1,16 +1,22 @@
 <?php get_header(); ?>
 
 
-<section id="intro" class='content-box dark row' data-speed="6" data-type="background">
-    <div class="content-box">
-        <img src='wp-content/themes/weny/images/banana.jpg' />
+<section id="intro" class='content-box row' data-speed="6" data-type="background">
+    <div class='arrow down'></div>
+    <div class="dark">
+        <!--<div class=''> <!-- col-md-8 col-md-offset-2 col-xs-offset-2 col-xs-8-->
+            <img src='wp-content/themes/weny/images/white_logo.png' />
+        <!--</div>-->
     </div>
+    <div class='arrow up'></div>
 </section>
 
-<section id="copy" class='content-box row' data-speed="6" data-type="background">
-    
-        <div class='inner'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum tellus quis nulla placerat dignissim. Ut at erat ut mi fringilla lacinia sed eu sem. Curabitur at ipsum at mi dictum sagittis. Fusce ullamcorper diam id pulvinar molestie. Cras erat tellus, porttitor at posuere vitae, dapibus sed orci. Duis sed risus lectus. Integer a ullamcorper massa, nec placerat lacus.
+<section id="copy" class='row' data-speed="6" data-type="background">
+    <div class='arrow down'></div>
+    <div class='inner'>
+        <div class='headline'>Our Story</div>
+        <div class='columns'>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum tellus quis nulla placerat dignissim. Ut at erat ut mi fringilla lacinia sed eu sem. Curabitur at ipsum at mi dictum sagittis. Fusce ullamcorper diam id pulvinar molestie. Cras erat tellus, porttitor at posuere vitae, dapibus sed orci. Duis sed risus lectus. Integer a ullamcorper massa, nec placerat lacus.
 
 Vivamus a metus condimentum, venenatis quam in, blandit elit. In hac habitasse platea dictumst. Nam tellus lectus, tristique vel odio eu, hendrerit vestibulum diam. Nulla facilisi. Nulla ultricies id leo a vestibulum. Duis id mi euismod, laoreet nisi ac, rutrum sem. Mauris nec venenatis libero. Maecenas rhoncus sed leo id efficitur. Phasellus quis est sit amet eros fermentum tempus.
 
@@ -21,19 +27,18 @@ Nam luctus aliquam felis, sed ultrices nisi egestas a. Nullam ornare auctor tort
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum tellus quis nulla placerat dignissim. Ut at erat ut mi fringilla lacinia sed eu sem. Curabitur at ipsum at mi dictum sagittis. Fusce ullamcorper diam id pulvinar molestie. Cras erat tellus, porttitor at posuere vitae, dapibus sed orci. Duis sed risus lectus. Integer a ullamcorper massa, nec placerat lacus.
 
 Vivamus a metus condimentum, venenatis quam in, blandit elit. In hac habitasse platea dictumst. Nam tellus lectus, tristique vel odio eu, hendrerit vestibulum diam. Nulla facilisi. Nulla ultricies id leo a vestibulum. Duis id mi euismod, laoreet nisi ac, rutrum sem. Mauris nec venenatis libero. Maecenas rhoncus sed leo id efficitur. Phasellus quis est sit amet eros fermentum tempus.
-
-Vivamus vitae elementum risus, sed faucibus urna. Vivamus condimentum mattis vehicula. Phasellus condimentum mattis blandit. Sed vehicula vehicula mollis. Nam non lacus eget massa euismod tempor nec mollis est. Cras sit amet urna at ipsum mollis efficitur. Duis ligula eros, mattis ac viverra eu, vehicula eget enim.
-
-Nam luctus aliquam felis, sed ultrices nisi egestas a. Nullam ornare auctor tortor ac dignissim. Nullam hendrerit eget turpis id ultrices. Pellentesque ut rhoncus mi. Proin at arcu odio. Donec ac suscipit sem. Morbi condimentum facilisis nulla, nec mollis felis vestibulum vitae. Maecenas id est lobortis massa aliquet dignissim. Vivamus blandit nisl at magna convallis, ut mollis nulla vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque dapibus lacinia mauris nec elementum. Fusce at nunc tortor. Aenean vehicula est vulputate arcu ultrices, id convallis ligula posuere. Donec suscipit id urna at vehicula. Sed gravida pretium fermentum.
-
-tum tempus.
-    
+        </div>
     </div>
+    <div class='arrow up'></div>
 </section>
 
-<section id="interact" class='content-box row dark' data-speed="6" data-type="background">
-    <div class='inner'>
-        Interact
+<section id="interact" class='content-box row' data-speed="6" data-type="background">
+    <div class='dark'>
+        <div class='headline'>Membership</div>
+        <div class='inner'>
+            Thank you for your interest in becoming a member!<br />
+            We will send you an email shortly detailing the next steps.
+        </div>
     </div>
 </section>
 
