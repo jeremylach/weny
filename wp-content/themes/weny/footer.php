c7abe4cc7eaa77ae719f6e0c95a4677022861ff6
Jeremy Lach <lach.jeremy@gmail.com>
2015-02-19 10:10:10 -1000

    removed extra theme files

--- wp-content/themes/weny/footer.php
+++ wp-content/themes/weny/footer.php
@@ -206,10 +206,11 @@ echo "BOGUS";
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '', 'auto');
+  ga('create', 'UA-59935226-1', 'auto');
   ga('send', 'pageview');
 
 	</script>
+
 
 </body>
 </html>
--- wp-content/themes/weny/searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php esc_url( home_url( '/' ) ); ?>">
-	<div>
-		<input type="text" value="<?php get_search_query(); ?>" name="s" id="s" />
-		<input type="submit" id="searchsubmit" value="Search" />
-	</div>
-</form>
